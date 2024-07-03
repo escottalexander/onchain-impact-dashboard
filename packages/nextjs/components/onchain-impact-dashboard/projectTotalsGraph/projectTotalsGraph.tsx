4292146e06a47b2601639912b5a540b7887c1693
escottalexander@gmail.com <escottalexander@gmail.com>
2024-07-03 16:53:57 -0400

    wip - tremor works on project details view

--- packages/nextjs/components/onchain-impact-dashboard/projectTotalsGraph/projectTotalsGraph.tsx
+++ packages/nextjs/components/onchain-impact-dashboard/projectTotalsGraph/projectTotalsGraph.tsx
@@ -2,9 +2,11 @@
 
 import React from "react";
 import { FilterButton } from "../filterButton/filterButton";
-import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { AreaChart } from "@tremor/react";
+// import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import { DatePicker } from "~~/components/impact-vector/inputs/datePicker";
-import { formatDate, stringToColor } from "~~/utils/onchainImpactDashboard/common";
+
+// import { formatDate, stringToColor } from "~~/utils/onchainImpactDashboard/common";
 
 interface IProps {
   id: string;
@@ -25,31 +27,31 @@ export const ProjectTotalsGraph = ({
   onFilter,
   endDate,
   selectedMetrics,
-  keys,
+  // keys,
   totalsRecord,
   filter,
 }: IProps) => {
-  const CustomTooltip = ({ active, payload, label }: any) => {
-    if (active && payload && payload.length) {
-      return (
-        <div className="bg-base-300 p-4 rounded-lg shadow">
-          <p className="m-0 p-0 label font-bold">Date</p>
-          <p className="m-0 p-0 label">{formatDate(label)}</p>
-          {payload.map((entry: any, index: any) => (
-            <div key={`item-${index}`}>
-              <p style={{ color: entry.color }} className="m-0 mt-4 p-0 label font-bold">
-                {entry.name}
-              </p>
-              <p style={{ color: entry.color }} className="m-0 p-0 label">
-                {entry.value}
-              </p>
-            </div>
-          ))}
-        </div>
-      );
-    }
-    return null;
-  };
+  // const CustomTooltip = ({ active, payload, label }: any) => {
+  //   if (active && payload && payload.length) {
+  //     return (
+  //       <div className="bg-base-300 p-4 rounded-lg shadow">
+  //         <p className="m-0 p-0 label font-bold">Date</p>
+  //         <p className="m-0 p-0 label">{formatDate(label)}</p>
+  //         {payload.map((entry: any, index: any) => (
+  //           <div key={`item-${index}`}>
+  //             <p style={{ color: entry.color }} className="m-0 mt-4 p-0 label font-bold">
+  //               {entry.name}
+  //             </p>
+  //             <p style={{ color: entry.color }} className="m-0 p-0 label">
+  //               {entry.value}
+  //             </p>
+  //           </div>
+  //         ))}
+  //       </div>
+  //     );
+  //   }
+  //   return null;
+  // };
 
   return (
     <div className="mb-3 w-full h-[50vh] rounded-lg grow min-h-[300px] lg:mr-4 lg:7/12 relative">
@@ -79,18 +81,37 @@ export const ProjectTotalsGraph = ({
           )}
         </div>
       </div>
-      <ResponsiveContainer width="100%" className={"absolute top-14"}>
+      {/* <ResponsiveContainer width="100%" className={"absolute top-14"}>
         <AreaChart data={totalsRecord} margin={{ top: 20, right: -16, bottom: 45, left: 0 }}>
           {keys
             .filter(key => selectedMetrics.includes(key))
-            .map(item => (
-              <Area key={item} type="monotone" dataKey={item} stroke={stringToColor(item)} fill={stringToColor(item)} />
-            ))}
+            .map(item => { console.log(item); return (
+              <Area
+                key={item}
+                stackId="1"
+                type="monotone"
+                dataKey={item}
+                stroke={stringToColor(item)}
+                fill={stringToColor(item)}
+              />
+            )})}
           <XAxis tick={false} fontSize={10} hide={false} dataKey="date" />
           <YAxis fontSize={10} type={"category"} hide={false} orientation="right" />
           <Tooltip content={<CustomTooltip />} />
         </AreaChart>
-      </ResponsiveContainer>
+      </ResponsiveContainer> */}
+      <AreaChart
+        className="h-100"
+        data={totalsRecord}
+        index="date"
+        categories={selectedMetrics}
+        colors={["indigo", "rose"]}
+        valueFormatter={v => v.toFixed(2)}
+        yAxisWidth={60}
+        stack={true}
+        showAnimation={true}
+        onValueChange={v => console.log(v)}
+      />
     </div>
   );
 };
